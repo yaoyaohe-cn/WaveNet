9ef1afe5ec95c35e09c864515ba5b9cafc6440c4
yaoyaohe-cn <108442516+yaoyaohe-cn@users.noreply.github.com>
2026-01-06 17:22:07 +0800

    Create ETTm2.sh

---
+++ scripts/Optuna/ETTm2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ ! -d "./logs/Optuna" ]; then
+    mkdir -p ./logs/Optuna
+fi
+
+export CUDA_VISIBLE_DEVICES=0
+
+dataset=ETTm2
+pred_len=96
+n_trials=60
+
+
+echo "Starting Comprehensive Tuning for ${dataset}..."
+
+python -u run_SWN_optuna.py \
+    --data $dataset \
+    --pred_len $pred_len \
+    --n_trials $n_trials \
+    --train_epochs 30 \
+    --patience 5 \
+    > logs/Optuna/tuning_v4_${dataset}_${pred_len}.log
+
+echo "Optimization Finished. Check logs/Optuna/tuning_${dataset}_${pred_len}.log"
